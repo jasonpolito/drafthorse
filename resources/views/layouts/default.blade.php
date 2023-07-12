38681916b8f3891c996b0930a01be1dda9e35f51
Jason Polito <jasonpolito@gmail.com>
2023-07-12 17:05:28 -0400

    sync

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,29 +1,25 @@
-<nav class="border-gray-200 bg-gray-50 dark:border-gray-700 dark:bg-gray-800">
-    <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
-        <a href="#" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <button data-collapse-toggle="navbar-solid-bg" type="button"
-                class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-                aria-controls="navbar-solid-bg" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                 viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-        </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-            <ul
-                class="flex flex-col mt-4 font-medium rounded-lg bg-gray-50 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-transparent md:dark:bg-transparent">
-                @foreach ($data->links as $link)
+<nav class="py-4 text-white bg-slate-800">
+    <x-container>
+        <div class="flex items-center justify-between">
+            <div>
+                // Logo
+            </div>
+            <div>
+                <ul class="flex">
+                    @foreach ($data->links as $link)
+                        <li>
+                            <a href="{{ $link->url }}"
+                               class="block px-4 py-2 rounded-md hover:bg-slate-900">{{ $link->text }}</a>
+                        </li>
+                    @endforeach
                     <li>
-                        <a href="{{ $link->url }}"
-                           class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">{{ $link->text }}</a>
+                        <a href="#" class="block px-4 py-2 rounded-md hover:bg-slate-900">
+                            <x-heroicon-o-menu />
+                        </a>
+
                     </li>
-                @endforeach
-            </ul>
+                </ul>
+            </div>
         </div>
-    </div>
+    </x-container>
 </nav>
